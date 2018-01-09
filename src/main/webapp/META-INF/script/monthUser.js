836d922a84220576d823aecd0ce3a35de9c14552
liumeng <liushuijiwusi@163.com>
2018-01-09 15:52:35 +0800

    a

--- src/main/webapp/META-INF/script/monthUser.js
+++ src/main/webapp/META-INF/script/monthUser.js
@@ -235,7 +235,9 @@ monthUserApp.controller("monthUserModify",function($scope, textModal,$modalInsta
     $scope.tempUser.endtime=new Date().format("yyyy-MM-dd hh:mm:ss");
     }   
     $scope.statuses=[{value:0,text:'未支付'},{value:1,text:'已支付'}];
-  
+    $scope.orderTypes=[{value:0,text:'包月用户'},{value:1,text:'类型A'},{value:2,text:'类型B'},{value:3,text:'类型C'},{value:4,text:'类型D'}];
+    $scope.tempUser.type=0;
+    
     $scope.parks=[];
     $scope.getParks=function(){
         $http({
